86dec9bd4acea0069725ec50189073b1ffddb01a
Veena_Mathew <Veena_Mathew@infosys.com>
2017-01-27 12:23:41 +0530

    removed the 1st code line

--- SeleniumPractice/src/GITHello.java
+++ SeleniumPractice/src/GITHello.java
@@ -3,7 +3,6 @@ public class GITHello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello git try");
 		System.out.println("Making a change");
 
 	}
