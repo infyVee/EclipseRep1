3e1d90956f7512859f800eb4422ad33092ff441c
Veena_Mathew <Veena_Mathew@infosys.com>
2017-01-30 09:51:06 +0530

    committing.

--- SeleniumPractice/src/GITHello.java
+++ SeleniumPractice/src/GITHello.java
@@ -4,6 +4,7 @@ public class GITHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Making a change okay ??");
+		System.out.println("change on 30th January,2017");
 
 	}
 
